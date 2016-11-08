0ab3a1b3b71a74844a56ef7f533b55a67bb8b7b9
Nathan Files <nathanwfiles@gmail.com>
2016-11-08 01:56:29 -0500

    use xunit to test set01

---
+++ test/Cryptopals.Tests/Set01.fs
@@ -0,0 +1,16 @@
+namespace Cryptopals.Tests
+
+module Set01Tests =
+    open Xunit
+    open System
+    open Encoding
+
+    let private hexToBase64String = seq >> Hex.decode >> Base64.encode >> TestHelpers.seqToString
+
+    [<Fact>]
+    let ConvertHexToBase64 input expected =
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+        let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+        let actual = hexToBase64String input
+        Assert.Equal(expected, actual, StringComparer.OrdinalIgnoreCase)
