2f2717bb26945168e214ad87f9b72803f981c809
Nathan Files <nathanwfiles@gmail.com>
2017-06-21 06:32:39 -0400

    implements single-byt XOR cipher
    - scores options based on character frequency
    - test produces expected result
    - very exciting :)

--- test/Cryptopals.Tests/Set01.fs
+++ test/Cryptopals.Tests/Set01.fs
@@ -6,7 +6,7 @@ open System.Collections.Generic
 open Cryptopals
 
 module Set01Tests =
-    let private hexToBase64String = seq >> Hex.decode >> Base64.encode >> TestHelpers.seqToString
+    let private hexToBase64String = Hex.decode >> Base64.encode >> String.Concat
 
     [<Fact>]
     let Challenge01ConvertHexToBase64() =
@@ -20,59 +20,39 @@ module Set01Tests =
     let Challenge02FixedXor() =
         let input = "1c0111001f010100061a024b53535009181c"
         let xorTarget = "686974207468652062756c6c277320657965"
-        let expected = seq "746865206b696420646f6e277420706c6179"
+        let expected = "746865206b696420646f6e277420706c6179"
 
         let actual =
-            Comparison.xorStreams (Hex.decode input) (Hex.decode xorTarget)
-                |> Hex.encode
+            Comparison.xorStreams
+                <| Hex.decode input
+                <| Hex.decode xorTarget
+            |> Hex.encode
+            |> String.Concat
 
-        Assert.Equal(expected |> TestHelpers.seqToString,
-                     actual   |> TestHelpers.seqToString,
-                     StringComparer.OrdinalIgnoreCase)
+        Assert.Equal(expected, actual, StringComparer.OrdinalIgnoreCase)
 
     [<Fact>]
     let Challenge03SingleByteXORCipher() =
-        let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+        let input = Hex.decode "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+        let expected = "Cooking MC's like a pound of bacon"
 
-        let bytes = Hex.decode input
+        let possibleKeys =
+            [
+                ['a'..'z']
+                ['A'..'Z']
+                ['0'..'9']
+            ]
+            |> Seq.collect (
+                String.Concat >> System.Text.Encoding.UTF8.GetBytes
+            )
 
-        let byteToChar (b:byte) = System.Text.Encoding.UTF8.GetChars([|b|]).[0]
-        let charToByte (c:char) = System.Text.Encoding.UTF8.GetBytes([|c|]).[0]
-        printfn "char-ed input bytes: %s" (bytes |> Seq.map (byteToChar >> string) |> String.concat "")
-
-        let options =
-            [['a'..'z'];['A'..'Z'];['0'..'9']]
-            |> Seq.concat |> Seq.map charToByte
-
-        printfn "options: %s" (options |> Seq.map (byteToChar >> string) |> String.concat "")
-        let c = Comparison.findBestXorByFrequency options bytes
-        printfn "Best XOR character: %s" (string c)
+        let actualKey = Comparison.findBestXorByFrequency possibleKeys input
 
         let cleartext =
-            Comparison.xorStreamWithSingleByte (byte c) bytes
-                |> Seq.map char
-                |> TestHelpers.seqToString
-        printfn "cleartext: %s" cleartext
+            Comparison.xorStreamWithSingleByte
+                <| actualKey
+                <| input
+            |> Seq.map char
+            |> String.Concat
 
-module ComparisonTests =
-    let characterFrequencyData: Object [] list =
-        [
-            [|
-                "hello"
-                [('H',0.2);('E',0.2);('L',0.4);('O',0.2)] |> dict
-            |]
-            [|
-                "tenletters"
-                [('T',0.3);('E',0.3);('N',0.1);('L',0.1);('S',0.1);('R',0.1)] |> dict
-            |]
-        ]
-
-    [<Theory>]
-    [<MemberData "characterFrequencyData">]
-    let CharacterFrequency(input, expected:IDictionary<char,double>) =
-        let getKey (i:KeyValuePair<_,_>) = i.Key
-        let sortDictionary = seq >> Seq.sortBy (getKey >> Char.ToUpper)
-        let actual = Comparison.characterFrequenciesInStream input |> dict
-
-        Seq.zip (sortDictionary expected) (sortDictionary actual)
-            |> Seq.iter (fun (a,b) -> Assert.Equal(a,b))
+        Assert.Equal(expected, cleartext)
--- test/Cryptopals.Tests/TestHelpers.fs
+++ test/Cryptopals.Tests/TestHelpers.fs
@@ -3,15 +3,13 @@ namespace Cryptopals.Tests
 module TestHelpers =
     open System
 
-    let seqToString = Seq.map string >> String.concat ""
-
     let stringifyBits (bytes:byte seq) =
         let getBits b =
             let bitToChar b = if b > byte 0 then '1' else '0'
             let getBit i = (b >>> i) &&& 0b1uy
             [0..7] |> Seq.rev |> Seq.map (getBit >> bitToChar)
         bytes
-            |> Seq.map (getBits >> seqToString)
+            |> Seq.map (getBits >> String.Concat)
             |> String.concat " "
 
     let stringCompare (comparison: StringComparison) s1 s2 =
