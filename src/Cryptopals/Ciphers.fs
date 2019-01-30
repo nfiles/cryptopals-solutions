2b476af95f157c13f59c16252f04ff2b38dd127a
Nathan Files <nathanwfiles@gmail.com>
2019-01-30 12:49:31 -0500

    solves set 1 problem 5!
    - implements repeating key XOR
    - moves XOR functions into the `Ciphers` module (better name pending)

--- src/Cryptopals/Ciphers.fs
+++ src/Cryptopals/Ciphers.fs
@@ -1,3 +1,12 @@
 namespace Cryptopals
 
-module Cipher =
+module Ciphers =
+    let xorStreams streamA streamB =
+        let xorBytePair (a: byte, b: byte) = a ^^^ b
+        Seq.zip streamA streamB |> Seq.map xorBytePair
+
+    let xorStreamWithSingleByte (b: byte) =
+        xorStreams <| Seq.initInfinite (fun _ -> b)
+
+    let xorStreamWithRepeatingKey (key: byte []) =
+        xorStreams <| Seq.initInfinite (fun i -> key.[i % key.Length])
--- src/Cryptopals/Comparison.fs
+++ src/Cryptopals/Comparison.fs
@@ -2,47 +2,34 @@ namespace Cryptopals
 
 module Comparison =
     open System.Collections.Generic
-    open System.IO
 
     type FrequencyMap = IDictionary<char, float>
 
-    let buildCorpus (text: string) =
+    let buildCorpus (text: string): FrequencyMap =
         let length = text.Length
         text
             |> Seq.countBy id
             |> Seq.map (fun (ch, count) -> (ch, (float count) / (float length)))
             |> dict
-        
-    let xorStreams streamA streamB =
-        let xorBytePair (a: byte, b: byte) = a ^^^ b
-        Seq.zip streamA streamB |> Seq.map xorBytePair
-
-    let xorStreamWithSingleByte b = Seq.initInfinite (fun _ -> b) |> xorStreams
 
     let scoreByCharacterFrequency (corpus: FrequencyMap) (stream: seq<char>) =
-        let filteredStream = stream
-
         let getExpectedFreq c =
             if corpus.ContainsKey(c)
             then corpus.[c]
             else float 0
 
-        let totalNumChars = filteredStream |> Seq.length
+        let totalNumChars = stream |> Seq.length
 
-        filteredStream
-            // count the occurrences of each character
-            |> Seq.countBy id
-            |> Seq.sumBy (fun (ch, count) ->
-                let expected = getExpectedFreq ch
-                expected * (float count)
-            )
+        stream
+            // sum the frequency scores of each character
+            |> Seq.sumBy getExpectedFreq
             // divide total score by size of the set
             |> fun f -> f / (float totalNumChars)
 
     let findBestXorByFrequency (corpus: FrequencyMap) (options: seq<byte>) (source: seq<byte>) =
-        /// Calculates the XOR of the stream and gets the frequency "score"
+        /// Calculates the XOR of the stream and gets the frequency score
         let xorAndScore (b: byte) =
-            xorStreamWithSingleByte b
+            Ciphers.xorStreamWithSingleByte b
             >> Seq.map char
             >> scoreByCharacterFrequency corpus
 
@@ -50,4 +37,3 @@ module Comparison =
             |> Seq.map (fun key -> (key, xorAndScore key source))
             |> Seq.sortByDescending (fun (_, score) -> score)
             |> Seq.head
-            |> fun (key, _) -> key
