ac2b9d402d4a5a2080eb60655500e0dea4f8ee07
Nathan Files <nathanwfiles@gmail.com>
2019-01-31 17:05:30 -0500

    working on problem 6

--- src/Cryptopals/Comparison.fs
+++ src/Cryptopals/Comparison.fs
@@ -1,4 +1,5 @@
 namespace Cryptopals
+open System
 
 module Comparison =
     open System.Collections.Generic
@@ -49,3 +50,78 @@ module Comparison =
             <| Seq.collect getBits stream2
         |> Seq.filter (fun (a, b) -> a <> b)
         |> Seq.length
+
+    let findBestRepeatingXorKey (corpus: FrequencyMap) (stream: byte[]) =
+        let totalLength = Seq.length stream
+
+        /// get the nth chunk of a certain size from the byte sequence
+        let getChunk num size (stream: byte[]) =
+            let chunkStart = num * size
+            let chunkEnd = Math.Min((num + 1) * size, stream.Length)
+
+            if stream.Length < chunkStart
+            then [||]
+            else stream
+                |> Seq.skip chunkStart
+                |> Seq.take (chunkEnd - chunkStart)
+                |> Seq.toArray
+        
+        // printfn "chunks:\n%s" ()
+
+        // 1. find the key length
+        let possibleKeyLengths = seq { 2..40 }
+        let bestKeyLength =
+            possibleKeyLengths
+            // don't consider keys longer than the input
+            |> Seq.filter (fun length -> length < totalLength)
+            |> Seq.map (fun length ->
+                let chunk1 = getChunk 0 length stream
+                let chunk2 = getChunk 1 length stream
+                let distance = getHammingDistance chunk1 chunk2
+                (distance, length)
+            )
+            |> Seq.sortBy (fun (distance, length) -> float distance / float length)
+            |> Seq.head
+            |> fun (_, length) -> length
+
+        printfn "best key length: %d" bestKeyLength
+
+        // 2. find the best key
+        let numChunks = (float totalLength) / (float bestKeyLength) |> Math.Ceiling |> int
+        let allChunks =
+            Seq.map
+                <| fun num -> getChunk num bestKeyLength stream |> Seq.toArray
+                <| seq { 0..numChunks }
+
+        // create a collection of chunks of the nth byte of each chunk
+        let verticalSlices =
+            seq { 0..numChunks }
+            |> Seq.map (fun num ->
+                allChunks
+                |> Seq.filter (fun chunk -> num < chunk.Length)
+                |> Seq.map (fun chunk -> chunk.[num])
+                // |> Seq.map (fun chunk ->
+                //     if num >= chunk.Length then None
+                //     else Some(chunk.[num])
+                // )
+                // |> Seq.filter Option.isSome
+                // |> Seq.map (fun option ->
+                //     match option with
+                //     | Some(b) -> b
+                //     | _ -> failwith "expected byte value"
+                // )
+                |> Seq.toArray
+            )
+
+        let bestKey =
+            verticalSlices
+            |> Seq.map (fun slice ->
+                findBestXorByFrequency
+                    <| corpus
+                    <| seq { (byte 0)..(byte 255) }
+                    <| slice
+                |> fst
+            )
+            |> Seq.toArray
+        
+        bestKey
---
+++ test/Cryptopals.Playground/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+open Cryptopals
+open Cryptopals.Tests
+open System.Text
+
+let private possibleKeys = seq { (byte 0)..(byte 255) }
+
+/// create a character frequency map from the sample text
+let corpus =
+    File.ReadAllText "./data/aliceinwonderland.txt"
+    |> Comparison.buildCorpus
+
+[<EntryPoint>]
+let main argv =
+    let input =
+        File.ReadAllText "./data/set01/6.txt"
+        |> Base64.decode
+        |> Seq.toArray
+    let key =
+        Comparison.findBestRepeatingXorKey
+            <| corpus
+            <| input
+        |> Seq.toArray
+
+    let decrypted =
+        Ciphers.xorStreamWithRepeatingKey key input
+        |> Seq.toArray
+        |> Encoding.ASCII.GetString
+    printfn "key: %s" (Encoding.ASCII.GetString key)
+    printfn "decrypted:"
+    printfn "%s" decrypted
+
+    0 // return an integer exit code
