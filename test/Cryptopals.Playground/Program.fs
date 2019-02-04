931940df3a22dd5cd4df7ca73020b1d3bfcfa391
Nathan Files <nathanwfiles@gmail.com>
2019-02-04 17:48:17 -0500

    more scratch work

--- test/Cryptopals.Playground/Program.fs
+++ test/Cryptopals.Playground/Program.fs
@@ -17,18 +17,23 @@ let main argv =
         File.ReadAllText "./data/set01/6.txt"
         |> Base64.decode
         |> Seq.toArray
-    let key =
-        Comparison.findBestRepeatingXorKey
-            <| corpus
-            <| input
-        |> Seq.toArray
+    
+    let keyLength = Comparison.findRepeatingXorKeyLength input
+    printfn "key length: %d" keyLength
 
-    let decrypted =
-        Ciphers.xorStreamWithRepeatingKey key input
-        |> Seq.toArray
-        |> Encoding.ASCII.GetString
-    printfn "key: %s" (Encoding.ASCII.GetString key)
-    printfn "decrypted:"
-    printfn "%s" decrypted
+    // let key =
+    //     Comparison.findBestRepeatingXorKey
+    //         <| corpus
+    //         <| keyLength
+    //         <| input
+    //     |> Seq.toArray
+
+    // let decrypted =
+    //     Ciphers.xorStreamWithRepeatingKey key input
+    //     |> Seq.toArray
+    //     |> Encoding.ASCII.GetString
+    // printfn "key: %s" (Encoding.ASCII.GetString key)
+    // printfn "decrypted:"
+    // printfn "%s" decrypted
 
     0 // return an integer exit code
